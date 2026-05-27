a85b6cf947fdd14f73fe377e44635ea27a0e35f3
Zain Balouch <zainbalouch159@gmail.com>
2026-05-27 08:50:15 +0500

    Improve Queue add functions

--- Queue.cpp
+++ Queue.cpp
@@ -11,10 +11,11 @@ class NODE{
 };
 
 class Queue{
+    
+    public:
     int data;
     NODE* front=nullptr;
     NODE* rear=nullptr;
-    public:
     void show(){
         if(front==nullptr){
             cout<<"Queue is empty"<<endl;
@@ -36,25 +37,79 @@ class Queue{
 
         if(rear==nullptr){
             rear=front=p;
-            front->next=rear;
-            rear->next=p->next;
             return;
         }
         
         rear->next=p;
         rear=p;
-        rear->data=p->data;
-        rear->next=p->next;
     }
-    void dequeue(){
-        
+    void dequeue(NODE* &front ){
+        if(front==nullptr){
+            cout<<"Nothing to delete"<<endl;
+            return;
+        }
+       NODE* p=front;
+       front=front->next;
+       if(front==nullptr){
+        rear=nullptr;
+       }
+       delete p;
+       cout<<"Deleted from front successfully"<<endl;
+        return;
+    }
+    void peek(){
+        if(front==nullptr){
+            cout<<"Queue is empty"<<endl;
+            return;
+        }
+        int pos=0,i=0;
+        cout<<"Enter position that you want to peek"<<endl;
+        cin>>pos;
+        NODE* p=front;
+        while(i!=pos){
+            i++;
+            p=p->next;
+            if(p==nullptr){
+                cout<<"That position don't exist"<<endl;
+                return;
+            }
+        }
+        cout<<i<<". "<<p->data<<endl;
     }
 };
 int main() {
     Queue z;
+   int choice;
+   do
+   {
+    cout<<"\n1. Show\n"
+        <<"2. Inqueue\n"
+        <<"3. Dequeue\n"
+        <<"4. peek\n"
+        <<"0. Exit"<<endl;
+        cin>>choice;
+    switch (choice)
+    {
+    case 1:
+        z.show();
+        break;
+    case 2:
     z.enqueue();
-    z.enqueue();
-    z.enqueue();
-    z.show();
+    break;
+    case 3:
+    z.dequeue(z.front);
+    break;
+    case 4:
+    z.peek();
+    break;
+    case 0:
+    cout<<"Bye"<<endl;
+    break;
+    default:
+    cout<<"Invalid Input"<<endl;
+        break;
+    }
+   } while (choice!=0);
+   
     return 0;
 }
