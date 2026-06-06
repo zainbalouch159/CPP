96a82621836d49d95784fd6c6c8edd596f18d168
Zain Balouch <zainbalouch159@gmail.com>
2026-06-06 12:17:41 +0500

    Orgainized

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,115 +1,131 @@
 #include <iostream>
 using namespace std;
-class NODE{
-    public:
+class NODE
+{
+public:
     int data;
-    NODE* next;
-    NODE(int val){
-        data=val;
-        next=nullptr;
+    NODE *next;
+    NODE(int value)
+    {
+        data = value;
+        next = nullptr;
     }
 };
 
-class Queue{
-    
-    public:
-    int data;
-    NODE* front=nullptr;
-    NODE* rear=nullptr;
-    void show(){
-        if(front==nullptr){
-            cout<<"Queue is empty"<<endl;
-            return;
-        }
-        NODE* p=front;
-        while(p!=nullptr){
-            cout<<p->data<<",";
-            p=p->next;
-        }
-        cout<<endl;
-        return;
-    }
-    void enqueue(){
-        int value;
-        cout<<"Enter data"<<endl;
-        cin>>value;
-        NODE* p=new NODE(value);
+class Queue
+{
 
-        if(rear==nullptr){
-            rear=front=p;
+public:
+    int data;
+    NODE *front = nullptr;
+    NODE *rear = nullptr;
+    void show()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empty" << endl;
             return;
         }
-        
-        rear->next=p;
-        rear=p;
-    }
-    void dequeue(NODE* &front ){
-        if(front==nullptr){
-            cout<<"Nothing to delete"<<endl;
-            return;
+        NODE *p = front;
+        while (p != nullptr)
+        {
+            cout << p->data << ",";
+            p = p->next;
         }
-       NODE* p=front;
-       front=front->next;
-       if(front==nullptr){
-        rear=nullptr;
-       }
-       delete p;
-       cout<<"Deleted from front successfully"<<endl;
+        cout << endl;
         return;
     }
-    void peek(){
-        if(front==nullptr){
-            cout<<"Queue is empty"<<endl;
+    void enqueue()
+    {
+        int value;
+        cout << "Enter data" << endl;
+        cin >> value;
+        NODE *p = new NODE(value);
+
+        if (rear == nullptr)
+        {
+            rear = front = p;
             return;
         }
-        int pos=0,i=0;
-        cout<<"Enter position that you want to peek"<<endl;
-        cin>>pos;
-        NODE* p=front;
-        while(i!=pos){
+
+        rear->next = p;
+        rear = p;
+    }
+    void dequeue(NODE *&front)
+    {
+        if (front == nullptr)
+        {
+            cout << "Nothing to delete" << endl;
+            return;
+        }
+        NODE *p = front;
+        front = front->next;
+        if (front == nullptr)
+        {
+            rear = nullptr;
+        }
+        delete p;
+        cout << "Deleted from front successfully" << endl;
+        return;
+    }
+    void peek()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        int pos = 0, i = 0;
+        cout << "Enter position that you want to peek" << endl;
+        cin >> pos;
+        NODE *p = front;
+        while (i != pos)
+        {
             i++;
-            p=p->next;
-            if(p==nullptr){
-                cout<<"That position don't exist"<<endl;
+            p = p->next;
+            if (p == nullptr)
+            {
+                cout << "That position don't exist" << endl;
                 return;
             }
         }
-        cout<<i<<". "<<p->data<<endl;
+        cout << i << ". " << p->data << endl;
     }
 };
-int main() {
+int main()
+{
     Queue z;
-   int choice;
-   do
-   {
-    cout<<"\n1. Show\n"
-        <<"2. Inqueue\n"
-        <<"3. Dequeue\n"
-        <<"4. peek\n"
-        <<"0. Exit"<<endl;
-        cin>>choice;
-    switch (choice)
+    int choice;
+    do
     {
-    case 1:
-        z.show();
-        break;
-    case 2:
-    z.enqueue();
-    break;
-    case 3:
-    z.dequeue(z.front);
-    break;
-    case 4:
-    z.peek();
-    break;
-    case 0:
-    cout<<"Bye"<<endl;
-    break;
-    default:
-    cout<<"Invalid Input"<<endl;
-        break;
-    }
-   } while (choice!=0);
-   
+        cout << "\n1. Show\n"
+             << "2. Inqueue\n"
+             << "3. Dequeue\n"
+             << "4. peek\n"
+             << "0. Exit" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            z.show();
+            break;
+        case 2:
+            z.enqueue();
+            break;
+        case 3:
+            z.dequeue(z.front);
+            break;
+        case 4:
+            z.peek();
+            break;
+        case 0:
+            cout << "Bye" << endl;
+            break;
+        default:
+            cout << "Invalid Input" << endl;
+            break;
+        }
+    } while (choice != 0);
+
     return 0;
 }
